b573dfea36b1560de5e0162dac47ba6ae5b6829d
Oleg <itsoldleg@gmail.com>
2024-10-03 19:13:07 +0500

    JFB-1: Added temporary storage(HashMap) for Ticket to TicketService

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,30 @@
+package Main;
+
+import Model.Ticket;
+import Service.TicketService;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    /*Scanner scanner = new Scanner(System.in);
+    TicketService eventTickets = new TicketService(10);
+
+    System.out.println("The list of all tickets:");
+    eventTickets.printAllTickets();
+
+    System.out.println("Please enter the ticket id:");
+    String inputID = scanner.nextLine();
+
+    Ticket targetTicket = eventTickets.getTicketById(inputID);
+    if (targetTicket != null) {
+      System.out.println("Ticket found: " + targetTicket);
+    } else {
+      System.out.println("Ticket with id - " + inputID + " was not found.");
+    }
+
+    scanner.close();*/
+
+  }
+}
