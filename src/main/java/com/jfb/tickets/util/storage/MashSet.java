a927f3fb9a526d0f1421dce3a583ac1d248e56a3
Oleg <itsoldleg@gmail.com>
2024-10-21 17:00:10 +0500

    Used the LinkedList to practice with a custom HashSet class.

--- src/main/java/com/jfb/tickets/util/storage/MashSet.java
+++ src/main/java/com/jfb/tickets/util/storage/MashSet.java
@@ -1,41 +1,108 @@
 package com.jfb.tickets.util.storage;
 
 import com.jfb.tickets.util.interfaces.Identifiable;
-import java.util.HashMap;
+import java.util.LinkedList;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
+@ToString
+@EqualsAndHashCode
 public class MashSet<T> implements Identifiable {
 
-  private final int CLASS_ID = generateClassId();
-  static final Object PRESENT = new Object();
+  private final int classId = generateClassId();
+  private static final float LOAD_FACTOR = 0.75f;
+  private static final int INITIAL_CAPACITY = 16;
 
-  private HashMap<T, Object> internalMap;
+  private LinkedList<T>[] buckets;
+  private int size;
+  private int capacity;
 
   public MashSet() {
-    internalMap = new HashMap<>();
+    this.capacity = INITIAL_CAPACITY;
+    this.buckets = new LinkedList[capacity];
+    this.size = 0;
   }
 
-  public void put(T o) {
-    internalMap.put(o, PRESENT);
+  private int hash(T key) {
+    return Math.abs(key.hashCode()) % capacity;
   }
 
-  public void delete(T o) {
-    internalMap.remove(o);
+  public void put(T key) {
+    if (contains(key)) {
+      return;
+    }
+
+    if ((double) size / capacity >= LOAD_FACTOR) {
+      resize();
+    }
+
+    int index = hash(key);
+    if (buckets[index] == null) {
+      buckets[index] = new LinkedList<>();
+    }
+    buckets[index].add(key);
+    size++;
   }
 
-  public boolean contains(T o) {
-    return internalMap.containsKey(o);
+  public boolean contains(T key) {
+    int index = hash(key);
+    LinkedList<T> bucket = buckets[index];
+    if (bucket != null) {
+      for (T element : bucket) {
+        if (element.equals(key)) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
-  public Iterable<T> iterator() {
-    return internalMap.keySet();
+  public boolean delete(T key) {
+    int index = hash(key);
+    LinkedList<T> bucket = buckets[index];
+    if (bucket != null) {
+      return bucket.remove(key);
+    }
+    return false;
+  }
+
+  private void resize() {
+    int newCapacity = capacity * 2;
+    LinkedList<T>[] newBuckets = new LinkedList[newCapacity];
+
+    for (LinkedList<T> bucket : buckets) {
+      if (bucket != null) {
+        for (T key : bucket) {
+          int newIndex = Math.abs(key.hashCode()) % newCapacity;
+          if (newBuckets[newIndex] == null) {
+            newBuckets[newIndex] = new LinkedList<>();
+          }
+          newBuckets[newIndex].add(key);
+        }
+      }
+    }
+
+    this.buckets = newBuckets;
+    this.capacity = newCapacity;
   }
 
   public int size() {
-    return internalMap.size();
+    return size;
+  }
+
+  public Iterable<T> iterator() {
+    LinkedList<T> allElements = new LinkedList<>();
+    for (LinkedList<T> bucket : buckets) {
+      if (bucket != null) {
+        allElements.addAll(bucket);
+      }
+    }
+    return allElements;
   }
 
   @Override
   public int getClassId() {
-    return this.CLASS_ID;
+    return this.classId;
   }
+
 }
